cfd85feba34286a1e35a31879667548b7eba2255
MD. Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2023-03-28 13:32:26 +0600

    Fix: Subscription Currency cent to doller
    Fix: Production Error
    
    Fix: Production Error

--- Resources/views/customer_fields_view.blade.php
+++ Resources/views/customer_fields_view.blade.php
@@ -23,9 +23,7 @@
                                 
                                 @foreach($invoices as $inv)
                                     @if($inv->id == $subscription->latest_invoice)
-                                        <span class="price">
-                                            {{$inv->currency_symbol.$inv->total}} 
-                                        </span> 
+                                        <span class="price">{{$inv->currency_symbol}}{{number_format($inv->total/100, 2)}}</span> 
                                             <span class="date-time">{{date('M d, h:i A', $inv->created)}}</span>
                                     @endif
                                 @endforeach
@@ -41,7 +39,7 @@
                     <p class="invoice-title">{{__('Payments')}}<p>
                     @forelse($invoices as $key => $invoice) 
                         <div class="invoice {{ $loop->first ? 'first-invoice' : ''}}" >  
-                            <span class="price">{{$inv->currency_symbol}}{{number_format($invoice->total/100, 2)}}</span> 
+                            <span class="price">{{$invoice->currency_symbol}}{{number_format($invoice->total/100, 2)}}</span> 
                             <span class="status">{{$invoice->status}}</span>
                             <a href="{{$invoice->invoice_pdf}}">#invoice</a>
                             <span class="date-time">{{date('M d, h:i A', $invoice->created)}}</span>
