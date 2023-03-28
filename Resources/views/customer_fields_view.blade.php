b3bb005b90fa5f3f67c2ee2c71df72a6c5f1c57e
MD. Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2023-03-28 14:56:34 +0600

    Fix: Ensure 5 invoice limit in backend and frontend

--- Resources/views/customer_fields_view.blade.php
+++ Resources/views/customer_fields_view.blade.php
@@ -24,7 +24,7 @@
                                 @foreach($invoices as $inv)
                                     @if($inv->id == $subscription->latest_invoice)
                                         <span class="price">{{$inv->currency_symbol}}{{number_format($inv->total/100, 2)}}</span> 
-                                            <span class="date-time">{{date('M d, h:i A', $inv->created)}}</span>
+                                            <span class="date-time">{{date('M d Y, h:i A', $inv->created)}}</span>
                                     @endif
                                 @endforeach
 
@@ -38,12 +38,14 @@
                 <div class="invoices">
                     <p class="invoice-title">{{__('Payments')}}<p>
                     @forelse($invoices as $key => $invoice) 
-                        <div class="invoice {{ $loop->first ? 'first-invoice' : ''}}" >  
-                            <span class="price">{{$invoice->currency_symbol}}{{number_format($invoice->total/100, 2)}}</span> 
-                            <span class="status">{{$invoice->status}}</span>
-                            <a href="{{$invoice->invoice_pdf}}">#invoice</a>
-                            <span class="date-time">{{date('M d, h:i A', $invoice->created)}}</span>
-                        </div> 
+                        @if($loop->iteration <= 5)
+                            <div class="invoice {{ $loop->first ? 'first-invoice' : ''}}" >  
+                                <span class="price">{{$invoice->currency_symbol}}{{number_format($invoice->total/100, 2)}}</span> 
+                                <span class="status">{{$invoice->status}}</span>
+                                <a href="{{$invoice->invoice_pdf}}">#invoice</a>
+                                <span class="date-time">{{date('M d Y, h:i A', $invoice->created)}}</span>
+                            </div> 
+                        @endif
                     @empty
                         <p>{{__('No Invoice found')}}</p>
                     @endforelse
